d79085f3c537bc886c57a20e2870b6e06fab5496
Francois le Roux <231256@virtualwindow.co.za>
2026-07-23 21:58:31 +0200

    chore: started coding the splash screen

---
+++ MonoFact/constants/Colors.ts
@@ -0,0 +1,17 @@
+export const Colors = {
+  primary: "#0B1F3A",
+  primaryLight: "#163A63",
+  primaryDark: "#071628",
+
+  background: "#F7F9FC",
+  surface: "#FFFFFF",
+
+  textPrimary: "#0B1F3A",
+  textSecondary: "#5C677D",
+
+  success: "#2ECC71",
+  danger: "#E74C3C",
+
+  border: "#E4E8EF",
+};
+
---
+++ MonoFact/constants/Typography.ts
@@ -0,0 +1,9 @@
+export const Typography = {
+    hero: 32,
+    title: 28,
+    heading: 24,
+    subheading: 20,
+    body: 16,
+    caption: 14,
+    small: 12,
+}
