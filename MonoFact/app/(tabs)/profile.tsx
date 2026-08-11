1c46ccafa7a5fc5b95ff3ac3993bfb54b4c24b63
Francois le Roux <francoislerouxdev@gmail.com>
2026-08-11 21:40:40 +0200

    Moved to profile screen
    Started removing the mock data on the profile screen, will complete it tomorrow.

--- MonoFact/app/(tabs)/profile.tsx
+++ MonoFact/app/(tabs)/profile.tsx
@@ -9,30 +9,68 @@ import ProfileStatCard from "@/components/cards/ProfileStatCard";
 import AchievementsCard from "@/components/cards/AchiementCard";
 import BottomNav from "@/components/navigation/BottomNav";
 
+import { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { auth, db } from "@/app/services/config";
+ 
 export default function ProfileScreen() {
+
+  
+  const [userData, setUserData] = useState<any>(null);
+  
+  useEffect(() => {
+    const loadUser = async () => {
+      const currentUser = auth.currentUser;
+      
+      if (!currentUser) return;
+      
+      const snapshot = await getDoc(doc(db, "users", currentUser.uid));
+      
+      if (snapshot.exists()) {
+        setUserData(snapshot.data());
+      }
+    };
+    
+    loadUser();
+  }, []);
+  
+  // 👇 Move this here
+  if (!userData) {
+    return null;
+  }
+  
+  if (!userData) return null;
 
   const stats = [
     {
       icon: Zap,
-      value: "4,820",
+      value: userData.xp.toString(),
       label: "Total XP",
     },
     {
       icon: Target,
-      value: "84%",
+      value: `${Math.round(
+        (userData.totalCorrect /
+          Math.max(
+            1,
+            userData.totalCorrect + userData.totalIncorrect
+          )) * 100
+      )}%`,
       label: "Accuracy",
     },
     {
       icon: Gamepad2,
-      value: "47",
+      value: userData.gamesPlayed.toString(),
       label: "Games Played",
     },
     {
       icon: FlaskConical,
-      value: "Science",
+      value: userData.favoriteCategory,
       label: "Fav. Category",
     },
   ];
+
+
 
   return (
     <SafeAreaView style={styles.container}>
@@ -43,9 +81,9 @@ export default function ProfileScreen() {
       >
 
         <ProfileHeader
-          username="John Doe"
-          email="john.doe@email.com"
-          level="Level 7 • Fact Explorer"
+          username={userData.username}
+          email={userData.email}
+          level={'Level ${userData.level} * Fact Expolorer'}
         />
 
         <View style={styles.statsContainer}>
